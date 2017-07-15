da9b462b3c6ceb5c8c84d03b4ee7566f52bbc8b3
vijaya.vasantha@gmail.com <fayaz0211@gmail.com>
2017-07-15 19:40:05 +0100

    added chrome driver and test to get title

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+/**
+ * Created by fayaz on 15/07/2017.
+ */
+public class GoogleTest {
+    @Test
+    public void myTest() {
+        System.setProperty("webdriver.chrome.driver", "//Users//fayaz//Downloads//chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        String title = driver.getTitle();
+        System.out.println(title);
+
+
+    }
+}
